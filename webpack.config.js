0cdccdd614b49c82b9f321cb05af9e24cb4f0e66
ChristianVisintin <christian.visintin1997@gmail.com>
2020-04-28 21:38:41 +0200

    Load images to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   module: {
@@ -7,36 +8,45 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
+            loader: "html-loader",
+          },
+        ],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpg|png|svg|gif|woff|woff2|ttf|otf)$/,
         use: {
-          loader: 'url-loader',
+          loader: "url-loader",
+          options: {
+            limit: 8096,
+          },
         },
-      }
-    ]
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+    new CopyWebpackPlugin([{ from: "src/assets", to: "assets/" }]),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 };
