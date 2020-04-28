3f398f04991d70456070c3095ba07f8aa690a563
ChristianVisintin <christian.visintin1997@gmail.com>
2020-04-28 21:39:02 +0200

    Slideshow now uses data from json

--- src/js/components/HomeSlideshow.jsx
+++ src/js/components/HomeSlideshow.jsx
@@ -1,21 +1,63 @@
 import React from "react";
 import { Carousel, Image } from "react-bootstrap";
 import styled from "styled-components";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-export default function HomeSlideshow(props) {
+//Actions
+import { fetchRecipes } from "../actions/recipeActions";
 
-  const slideshow = props.images.map((image, index) => 
-    <Carousel.Item key={index}>
-      <Image className="d-block w-100" src={image.default} />
-      <Carousel.Caption>
-        <h1 style={{textShadow: "0px 0 black, 0 0.3px black, 1px 0 black, 0 0 black"}}>Lorem ipsum dolor sit amet</h1>
-      </Carousel.Caption>
-    </Carousel.Item>
-  );
+class HomeSlideshow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Carousel className="border-right border-left border-top d-block w-50" interval="5000">
-      {slideshow}
-    </Carousel>
-  );
+  componentDidMount() {
+    this.props.fetchRecipes();
+  }
+
+  render() {
+    //Get first 3 (or less) random recipes
+    let recipes = [];
+    let sortedIndex = [];
+    for (let i = 0; i < 3 && i < this.props.recipes.length; i++) {
+      let randomIndex = Math.floor(Math.random() * this.props.recipes.length);
+      //Index can't get sorted twice
+      if (sortedIndex.includes(randomIndex)) {
+        i--;
+        continue;
+      }
+      sortedIndex.push(randomIndex);
+      recipes.push(this.props.recipes[randomIndex]);
+    }
+    const slideshow = recipes.map((recipe) => (
+      <Carousel.Item key={recipe.id}>
+        <a href={"/#/recipe/" + recipe.id}>
+          <Image className="d-block w-100" src={recipe.img} />
+          <Carousel.Caption>
+            <h1 style={{textShadow: "0px 0 black, 0 0.3px black, 1px 0 black, 0 0 black"}}>
+              {recipe.title}
+            </h1>
+          </Carousel.Caption>
+        </a>
+      </Carousel.Item>
+    ));
+
+    return (
+      <Carousel className="border-right border-left border-top d-block w-50" interval="5000">
+        {slideshow}
+      </Carousel>
+    );
+  }
 }
+
+HomeSlideshow.propTypes = {
+  fetchRecipes: PropTypes.func.isRequired,
+  recipes: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  recipes: state.recipes.items,
+});
+
+export default connect(mapStateToProps, { fetchRecipes })(HomeSlideshow);
--- src/js/components/pages/Home.jsx
+++ src/js/components/pages/Home.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
-import importFrom from '../../utils';
-
-//Import images for slideshow
-const images = importFrom(require.context('../../../assets/images/contents/', false, /\.(png|jpe?g|svg)$/));
 
 //Layouts
 import HomeSlideshow from '../HomeSlideshow';
@@ -12,7 +8,7 @@ export default function Home() {
   return (
     <Container fluid className="w-100">
       <Container fluid className="justify-content-center row d-flex w-100">
-        <HomeSlideshow images={images} />
+        <HomeSlideshow />
       </Container>
     </Container>
   )
