25fa1df5a6541b3f8ed0bf4dcbdfaab56f16ab30
ChristianVisintin <christian.visintin1997@gmail.com>
2020-04-29 13:34:00 +0200

    ReactLazy for layouts; Waiting component

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,17 @@ import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
 //Redux
 import { Provider } from "react-redux";
 import store from "./js/store/index";
-//Bootstrap CSS
+//Bootstrap CSS (@! Use CDN instead - see index.html)
 //import "bootstrap/dist/css/bootstrap.min.css";
 
 //Components
 import Menu from "./js/components/layouts/Menu";
 import Footer from "./js/components/layouts/Footer";
+import Waiting from "./js/components/Waiting";
 //Pages
-import About from "./js/components/pages/About";
-import Home from "./js/components/pages/Home";
-import Recipes from "./js/components/pages/Recipes";
+const About = React.lazy(() => import("./js/components/pages/About"));
+const Home = React.lazy(() => import("./js/components/pages/Home"));
+const Recipes = React.lazy(() => import("./js/components/pages/Recipes"));
 
 export default class App extends Component {
   render() {
@@ -36,10 +37,12 @@ export default class App extends Component {
                   {/* HashRouter for page to display */}
                   <HashRouter>
                     <main className="page-content">
-                      <Route path="/about" component={About} />
-                      <Route path="/home" component={Home} />
-                      <Route exact path="/" component={Home} />
-                      <Route path="/recipes" component={Recipes} />
+                      <React.Suspense fallback={<Waiting />}>
+                        <Route path="/about" component={About} />
+                        <Route path="/home" component={Home} />
+                        <Route exact path="/" component={Home} />
+                        <Route path="/recipes" component={Recipes} />
+                      </React.Suspense>
                     </main>
                   </HashRouter>
                   {/*Footer is visible for all pages in / */}
