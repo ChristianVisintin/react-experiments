7a16a887e7f5004458eeeaf49d145320f480ff90
ChristianVisintin <christian.visintin1997@gmail.com>
2020-04-29 21:50:58 +0200

    Returns promise from Action

--- src/js/actions/recipeActions.js
+++ src/js/actions/recipeActions.js
@@ -1,31 +1,34 @@
-import { FETCH_RECIPES } from './types';
+import { FETCH_RECIPES } from "./types";
 
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 /**
  * @function fetchRecipes
  * @description fetch recipes on remote server and dispatch to store
  */
 
-export const fetchRecipes = () => dispatch => {
-  axios.get("http://localhost:3000/recipes").then((response) => {
-    const recipes = response.data;
-    recipes.sort((a, b) => {
-      const dateA = new Date(a.date);
-      const dateB = new Date(b.date);
-      if (dateA < dateB) return 1;
-      if (dateA > dateB) return -1;
-      return 0;
+export const fetchRecipes = () => (dispatch) => {
+  return axios
+    .get("http://localhost:3000/recipes")
+    .then((response) => {
+      const recipes = response.data;
+      recipes.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        if (dateA < dateB) return 1;
+        if (dateA > dateB) return -1;
+        return 0;
+      });
+      dispatch({
+        type: FETCH_RECIPES,
+        payload: recipes,
+      });
+    })
+    .catch((error) => {
+      console.error("Could not fetch recipes", error.data);
+      dispatch({
+        type: FETCH_RECIPES,
+        payload: [],
+      });
     });
-    dispatch({
-      type: FETCH_RECIPES,
-      payload: recipes
-    })
-  }).catch((error) => {
-    console.error("Could not fetch recipes", error.data);
-    dispatch({
-      type: FETCH_RECIPES,
-      payload: []
-    })
-  })
-}
+};
